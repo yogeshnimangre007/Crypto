70fd9c7e6646883ab0015084b590e7332c4913aa
Aditya Mishra <mishraaaditya29@gmail.com>
2021-04-20 12:53:04 +0530

    Profile Handler Function Removed

--- GoCrypto/Controller/controller.go
+++ GoCrypto/Controller/controller.go
@@ -51,32 +51,9 @@ func LoginHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 
-func ProfileHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	tokenString := r.Header.Get("Authorization")
-	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
-		// Don't forget to validate the alg is what you expect:
-		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
-			return nil, fmt.Errorf("Unexpected signing method")
-		}
-		return []byte("secret"), nil
-	})
-	var result model.User
-	var res model.ResponseResult
-	if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
-		result.Username = claims["username"].(string)
-		result.FirstName = claims["firstname"].(string)
-		result.LastName = claims["lastname"].(string)
+/*
 
-		json.NewEncoder(w).Encode(result)
-		return
-	} else {
-		res.Error = err.Error()
-		json.NewEncoder(w).Encode(res)
-		return
-	}
-
-}
+*/
 
 func CreateEvent(c echo.context) error {
 	e := new(Event)
