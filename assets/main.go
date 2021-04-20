4162cd90ff154dce2395f04f5f80d6a6c54413bf
himanshu07070 <36814920+himanshu07070@users.noreply.github.com>
2021-04-20 12:56:50 +0530

    main.go of assets

---
+++ assets/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"time"
+	"github.com/gorilla/mux"
+	"F:/assets/model"
+	"F:/assets/controller"
+
+)
+
+
+
+func handleRequest() {
+	new := mux.NewRouter().StrictSlash(true)
+	new.HandleFunc("/", homepage)
+	new.HandleFunc("/returnAll", returnAllAssets)
+	new.HandleFunc("/create", createNewAsset).Methods("POST")
+	new.HandleFunc("/update/{id}", updateAsset).Methods("PUT")
+	new.HandleFunc("/delete/{id}", deleteAsset).Methods("DELETE")
+	new.HandleFunc("/return/{id}", returnSingleAsset)
+	log.Fatal(http.ListenAndServe(":10000", new))
+}
+
+//main function which will kick off our API.
+func main() {
+	fmt.Println("crypto")
+	handleRequest()
+}
